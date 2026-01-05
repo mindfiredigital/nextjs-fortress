0cf0df4ade4d93284a8cb1d03770d5aef7ed3e4b
ParasMindfire <mfsi.parasb@outlook.com>
2026-01-05 16:40:35 +0530

    refactor: format whiteList

--- packages/nextjs-fortress/src/middleware/whiteList.ts
+++ packages/nextjs-fortress/src/middleware/whiteList.ts
@@ -1,20 +1,20 @@
 import { NextRequest } from 'next/server'
 import { FortressConfig } from '../types'
- 
+
 /**
  * Whitelist checker - isolated whitelist logic
  */
 export class WhitelistChecker {
   private config: FortressConfig
- 
+
   constructor(config: FortressConfig) {
     this.config = config
   }
- 
+
   isWhitelisted(request: NextRequest): boolean {
     const path = request.nextUrl.pathname
     const ip = request.headers.get('x-forwarded-for') || '127.0.0.1'
- 
+
     // Check whitelisted paths
     if (this.config.whitelist?.paths) {
       for (const whitelistedPath of this.config.whitelist.paths) {
@@ -23,12 +23,12 @@ export class WhitelistChecker {
         }
       }
     }
- 
+
     // Check whitelisted IPs
     if (this.config.whitelist?.ips && this.config.whitelist.ips.includes(ip)) {
       return true
     }
- 
+
     return false
   }
 }
